34ee0255f29101bf273e14dc0778a9f7098c84e3
nobita851 <rktipu1995@gmail.com>
2022-01-19 16:20:57 +0530

    eths

--- eths.js
+++ eths.js
@@ -1,3 +1,4 @@
+require("dotenv").config();
 const { ethers } = require("ethers");
 
 const WBNB = require("./build/contracts/wrappedBNB.json");
@@ -5,7 +6,7 @@ const Swap = require("./build/contracts/swap.json");
 
 const bscProvider = new ethers.providers.JsonRpcProvider(
   "https://speedy-nodes-nyc.moralis.io/a61a52180976b08f4ff81c83/bsc/testnet",
-  { name: "bscTestnet", chainId: 97 }
+  { name: "bscTestnet", chainId: 97, confirmations: 10 }
 );
 
 const user = {
@@ -21,8 +22,8 @@ const deployer = {
 const deployerWallet = new ethers.Wallet(deployer.privateKey, bscProvider);
 const userWallet = new ethers.Wallet(user.privateKey, bscProvider);
 
-const overrides = {
-  gasLimit: 21000,
+const override = {
+  gasLimit: 400000,
   gasPrice: ethers.utils.parseUnits("8.0", "gwei"),
   value: ethers.utils.parseUnits("0.2", "ether"),
 };
@@ -40,27 +41,24 @@ const factory2 = new ethers.ContractFactory(
 );
 
 async function run() {
-  const deployWBNB = await factory.deploy();
-  const contractWBNB = await deployWBNB.deployed();
+  const contractWBNB = await factory.deploy();
+  await contractWBNB.deployed();
 
-  console.log(
-    `Transaction for deploying wBNB = ${contractWBNB.deployTransaction}`
-  );
+  console.log(`Address wBNB = ${contractWBNB.address}`);
 
-  const deploySwap = factory2.deploy(contractWBNB.address);
-  const contractSwap = await deploySwap.deployed();
+  const contractSwap = await factory2.deploy(contractWBNB.address);
+  await contractSwap.deployed();
 
-  console.log(
-    `Transaction for deploying Swap = ${contractSwap.deployTransaction}`
-  );
+  console.log(`Address Swap = ${contractSwap.address}`);
 
   const userContract = new ethers.Contract(
     contractSwap.address,
     Swap.abi,
     userWallet
   );
-
-  let tx = await userContract.swapTokens(overrides);
+  // const gas = await contractSwap.estimateGas.swapTokens(override);
+  // console.log(`Gas = ${gas}`);
+  let tx = await userContract.swapTokens(override);
   let txComplete = await tx.wait();
   console.log(
     `https://explorer.bitquery.io/bsc_testnet/tx/${txComplete.transactionHash}`
